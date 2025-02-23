72a315ae7da5c18784771e463cd81074b59b759c
redparham <5248696+redparham@users.noreply.github.com>
2025-02-23 01:31:35 +0000

    add lists for todo, inprogress and done with drag and drop support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CdkDropList, CdkDrag],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'tasks-frontend-angular';
+
+  todo: string[] = ['Task A', 'Task B', 'Task C', 'Task D'];
+  inprogress: string[] = [];
+  done: string[] = [];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
