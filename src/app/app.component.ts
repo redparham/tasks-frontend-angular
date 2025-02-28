c7a8a4c14f53a3260f5549b82048e3a1e739a43f
redparham <5248696+redparham@users.noreply.github.com>
2025-02-28 15:15:03 +0000

    add spinner so user knows when data is loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {
   CdkDragDrop,
@@ -8,30 +8,38 @@ import {
   CdkDropList,
 } from '@angular/cdk/drag-drop';
 import { Task, TasksService, TaskStatus } from './tasks.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CdkDropList, CdkDrag],
+  imports: [RouterOutlet, CdkDropList, CdkDrag, MatProgressSpinnerModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tasks-frontend-angular';
 
   todo: Task[] = [];
   inprogress: Task[] = [];
   done: Task[] = [];
 
-  constructor(private tasksService: TasksService) {
-    var tasks = tasksService.getTasks()
+  loading = false;
+
+  constructor(private tasksService: TasksService) {}
+  
+  ngOnInit(): void {
+    this.loading = true;
+    var tasks = this.tasksService.getTasks()
       .subscribe(tasks => {
         this.todo = tasks.filter(task => task.status === TaskStatus.TODO);
         this.inprogress = tasks.filter(task => task.status  as TaskStatus === TaskStatus.INPROGRESS);
         this.done = tasks.filter(task => task.status  as TaskStatus === TaskStatus.DONE);
       });
-
+    this.loading=false;
   }
 
+  
   drop(event: CdkDragDrop<Task[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
